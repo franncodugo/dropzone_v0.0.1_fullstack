4c5d1a177d5644a37d1bda3e683c59843568c427
Dugo, Franco <FDugo@HudsonInsGroup.com>
2025-11-11 21:58:45 -0300

    Creating folders, components, fixing routes, building succ

--- src/dropzone_mvp_application/dropzone/src/app/app.routes.server.ts
+++ src/dropzone_mvp_application/dropzone/src/app/app.routes.server.ts
@@ -1,8 +1,18 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
-export const serverRoutes: ServerRoute[] = [
+const serverRoutes: ServerRoute[] = [
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'videos', renderMode: RenderMode.Prerender },
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: 'video/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => [
+      { id: '1' },
+      { id: '2' },
+      { id: '3' },
+    ],
+  },
+  { path: '**', renderMode: RenderMode.Client },
 ];
+
+export default serverRoutes;
--- src/dropzone_mvp_application/dropzone/src/app/app.routes.ts
+++ src/dropzone_mvp_application/dropzone/src/app/app.routes.ts
@@ -1,8 +1,22 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent,
-    },
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'videos',
+    loadComponent: () =>
+      import('./pages/video-list/video-list').then((m) => m.VideoList),
+  },
+  {
+    path: 'video/:id',
+    loadComponent: () =>
+      import('./pages/video-detail/video-detail').then((m) => m.VideoDetail),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
